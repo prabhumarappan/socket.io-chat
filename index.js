2350c6bad845aa06c02a2ecb2a8fdef252d439d1
Prabhu Marappan <prabhu@dozee.io>
2017-11-02 19:30:36 +0530

    adding redis for message storage

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var redis = require('redis');
 
 var http = require('http').Server(app);
 
@@ -9,23 +10,43 @@ app.get('/',function(req,res){
     res.sendFile(__dirname+'/index.html');
 });
 
+redis_client = redis.createClient();
+
+messages = []
 users = []
+
+redis_client.get('messages',function(err,reply){
+    if(reply){
+        messages = JSON.parse(reply);
+    }
+});
+redis_client.get('users',function(err,reply){
+    if(reply){
+        users = JSON.parse(reply);
+    }
+});
 
 io.on('connection',function(socket){
     console.log("A users connected!");
-    socket.broadcast.emit('newjoin');
+    socket.broadcast.emit('newjoin',{});
+    socket.on('ready',function(){
+        socket.emit('chat message',messages);
+    });
 
     socket.on('send-nickname',function(nickname){
         socket.username = nickname;
         users.push(socket.username);
+        redis_client.set('users',JSON.stringify(users));
     });
 
     socket.on('disconnect',function(){
         console.log("A user disconnected!");
     });
     socket.on('chat message',function(data){
-        socket.emit('chat message',"you say: "+data);
-        socket.broadcast.emit('chat message',socket.username +" says : "+data);
+        io.emit('chat message',[data]);
+        person = socket.username;
+        messages.push(data);
+        redis_client.set('messages',JSON.stringify(messages));
     });
 });
 
