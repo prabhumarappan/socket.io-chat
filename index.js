f9d7c834e7343d3ecad44962ef04d066b6ee05ba
Prabhu Marappan <prabhu@dozee.io>
2017-11-08 04:19:33 +0530

    Adding Channel based Chats

--- index.js
+++ index.js
@@ -6,45 +6,61 @@ var http = require('http').Server(app);
 
 var io = require('socket.io')(http);
 
+global.room_name;
+
+// app.get('',function(req,res){
+//     // res.sendStatus(500);
+//     res.send('Add a channel to the URL where you want to join!');
+// });
+
 app.get('/',function(req,res){
     res.sendFile(__dirname+'/index.html');
 });
 
 redis_client = redis.createClient();
 
-messages = []
-users = []
-
-redis_client.get('messages',function(err,reply){
-    if(reply){
-        messages = JSON.parse(reply);
-    }
-});
-redis_client.get('users',function(err,reply){
-    if(reply){
-        users = JSON.parse(reply);
-    }
-});
-
 io.on('connection',function(socket){
-    socket.on('ready',function(){
+    global.messages = [];
+    global.users = [];
+
+    socket.on('ready',function(message){
         socket.emit('chat message',messages);
+        console.log()
     });
 
     socket.on('send-nickname',function(nickname){
         socket.username = nickname;
         users.push(socket.username);
         console.log(nickname + " connected!");
-        socket.broadcast.emit('newjoin',nickname);
-        redis_client.set('users',JSON.stringify(users));
+        var keys = Object.keys(socket.rooms);
+        channel = socket.rooms[keys[1]];
+        socket.broadcast.to(socket.rooms[keys[1]]).emit('newjoin',nickname);
+        redis_client.set(channel+':users',JSON.stringify(users));
+    });
+
+    socket.on('join-channel',function(channel){
+        socket.join(channel);
+        //Load previous messages
+        redis_client.get(channel+':messages',function(err,reply){
+            if(reply){
+                global.messages = JSON.parse(reply);
+            }
+        });
+        redis_client.get(channel+':users',function(err,reply){
+            if(reply){
+                global.users = JSON.parse(reply);
+            }
+        });
     });
 
     socket.on('typing',function(){
-        socket.broadcast.emit('typing',socket.username+" is typing...");
+        var keys = Object.keys(socket.rooms);
+        socket.broadcast.to(socket.rooms[keys[1]]).emit('typing',socket.username+" is typing...");
     });
 
     socket.on('finished-typing',function(){
-        socket.broadcast.emit('typing');
+        var keys = Object.keys(socket.rooms);
+        socket.broadcast.to(socket.rooms[keys[1]]).emit('typing');
     })
 
     socket.on('disconnect',function(){
@@ -53,13 +69,16 @@ io.on('connection',function(socket){
     socket.on('chat message',function(data){
         person = socket.username;
         current_message  = {};
+        var keys = Object.keys(socket.rooms);
+        channel = socket.rooms[keys[1]];
+        // console.log(socket.rooms[keys[0]]);
         current_message['user'] = person;
         current_message['message'] = data;
         new_message = [];
         new_message.push(current_message);
         messages.push(current_message);
-        socket.broadcast.emit('chat message',new_message);
-        redis_client.set('messages',JSON.stringify(messages));
+        socket.broadcast.to(channel).emit('chat message',new_message);
+        redis_client.set(channel+':messages',JSON.stringify(messages));
     });
 });
 
